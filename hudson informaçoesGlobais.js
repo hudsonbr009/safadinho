554eb3567280593d4cc51b6b6de5d4243c9e7ac2
hudsonbr009 <110686679+hudsonbr009@users.noreply.github.com>
2024-11-05 10:51:34 -0300

    Add files via upload

--- hudson informaçoesGlobais.js
+++ hudson informaçoesGlobais.js
@@ -4,11 +4,22 @@ console.log(url);
 async function visualizarInformacoesGlobais() {
 const res = await fetch(url);
 const dados = await res.json()
-console.log(dados.tempo_medio);
+const pessoasConectadas = (dados.total_pessoas_ conectadas) / 1e9
+const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
+const horas = parseInt(dados.tempo_medio)
+const minutos = Math.round((dados.tempo_medio - horas)* 100)
+const porcentagemConectada = ((pessoasConectadas /pessoasNoMundo) * 100).toFixed(2)
+const paragrafo = document.createElement('p')
 }
 visualizarInformacoesGlobais()
 {
-    "total_pessoas_conectadas": 5.04e9,
-    "tempo_medio": 2.38,
-    "total_pessoas_mundo": 7.888e9
+    "total_pessoas_conectadas"; 5.04e9,
+    "tempo_medio"; 2.38,
+    "total_pessoas_mundo"; 7.888e9
     }
+    paragrafo.innerHTML = 'Você sabia que o mundo tem
+<span>${pessoaNosMundo} bilhões</span> de pessoas e
+que aproximadamente <span>${pessoasConectadas} bilhões </span> estão conectadas em alguma rede
+social e passam em media<span>${horas}</span> horas e <span>${minutos}</span> minutos conectadas.'
+<br> Isso significa que aproximadamente ${porcentagemConectada}%
+de pessoas estão conectadas em alguma rede social.'
